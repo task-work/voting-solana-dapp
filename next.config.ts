5963007555ae46bd372030bb8df72e9811c9175c
garysen <garysen@163.com>
2025-12-26 18:11:23 +0800

    remote /anchor/target from github

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,4 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
+/** eslint-disable @typescript-eslint/no-explicit-any */
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
